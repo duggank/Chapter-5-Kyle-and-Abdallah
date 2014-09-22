f6bfdd773d721b650009427c156508701c7cc22b
duggank <CreativeWeep@gmail.com>
2014-09-22 08:41:42 -0400

    Initial Commit V1
    Finished exercises 1 and 2.

---
+++ Chapter5/CheckZips/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CheckZips
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] zips = { 18045, 12345, 47234, 51236, 72130, 18040, 19204, 73214, 80102, 90210 };
+
+            bool Chosen = false;
+
+            Console.Write("Enter your zipcode: ");
+            string input = Console.ReadLine();
+            int userChoice = Convert.ToInt32(input);
+
+            for (int i = 0; i < zips.Length; i++)
+            {
+                if (userChoice == zips[i])
+                {
+                    Chosen = true;
+                }
+            }
+            if (Chosen)
+                Console.WriteLine("Alright, I'll send a package straight to yo house. ");
+            else
+                Console.WriteLine("Not a valid zip code... Please try again.");
+        }
+    }
+}
---
+++ Chapter5/IntegerList/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IntegerList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 10, 20, 30, 40, 50 };
+
+            Console.WriteLine("These are the array values from first to last: ");
+            Console.WriteLine("_____________________");
+            Console.WriteLine("");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Console.WriteLine("_____________________");
+            Console.WriteLine("");
+            Console.WriteLine("These are the array values from last to first:");
+            Console.WriteLine("_____________________");
+            Console.WriteLine("");
+
+            for (int j = array.Length-1; j >= 0; j--)
+            {
+                Console.WriteLine(array[j]);
+            }
+            Console.WriteLine("_____________________");
+        }
+    }
+}
